b672561da6e9d9c8739b15d72d1c663f637beac3
bgcasey <brendangerardcasey@gmail.com>
2026-02-18 04:05:42 -0700

    Add demo script for theme_science visualization

---
+++ demo/theme_science.R
@@ -0,0 +1,18 @@
+# Demo: theme_science
+
+library(ggplot2)
+library(sciCentRverse)
+
+base_plot <- ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_point(color = "steelblue", size = 2) +
+  geom_smooth(method = "lm", se = FALSE, color = "gray40") +
+  labs(
+    title = "Fuel Efficiency vs Weight",
+    subtitle = "Demo of theme_science",
+    x = "Weight (1000 lbs)",
+    y = "Miles per gallon",
+    caption = "Data: mtcars"
+  )
+
+print(base_plot)
+print(base_plot + theme_science())
